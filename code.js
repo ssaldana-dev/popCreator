a497c314119adac9b272a5a0c5f38c27fee79746
Sergio Saldaña <ssaldana.dev@gmail.com>
2023-05-20 07:22:44 -0600

    Added the text draw functions on canvas, and created a text justification algorithm

--- code.js
+++ code.js
@@ -9,39 +9,84 @@ background.src = './pops-base.jpg'
 
 ctx.textAlign = 'center';
 
-let sku = 'SKU: ' + '100100762';
-let productName = 'Laptop Asus VivoBook 15 X515EA-BQ2595W';
+let sku = 'SKU: ' + '100084694';
+let productName = 'Marc Permanentes Sharpie';
+let productDescription = 'Colección Místicos / Punta fina / Colores surtidos';
+let regularPrice = 253;
+let promotionalPrice = 151.80;
 
 background.onload = function () {
     ctx.drawImage(background, 0, 0);
+    writeStaticPhrases();
     writeSKU();
     writeProductName();
+    writeRegularPrice();
+    writePromotionalPrice();
 }
 
+function writeStaticPhrases () {
+    ctx.font = '70px Helvetica';
+    ctx.fillText('Precio especial', 476, 200);
+    ctx.fillText('De', 476, 515);
+    ctx.fillText('A', 476, 700);
+}
 function writeSKU () {
-    ctx.font = '75px Helvetica';
-    ctx.fillText(sku, 476, 250);
+    ctx.font = '55px Helvetica';
+    ctx.fillText(sku, 476, 270);
 }
 function writeProductName () {
     ctx.font = '75px Helvetica';
-    textJustification(productName, 476, 340, popWidth, lineHeight);
-    // ctx.fillText(productName, 476, 340);
+    justifyText(productName, lineHeight, 476, 350);
+}
+function writeRegularPrice () {
+    ctx.font = '90px Helvetica';
+    ctx.fillText(regularPrice.toFixed(2).toString(), 476, 615);
+    ctx.lineWidth = 15;
+    ctx.moveTo(250,620);
+    ctx.lineTo(685,540);
+    // ctx.strokeStyle = 'red';
+    ctx.stroke();
+}
+function writePromotionalPrice () {
+    ctx.font = 'bold 175px Helvetica';
+    ctx.fillStyle = 'red';
+    ctx.fillText(promotionalPrice.toFixed(2).toString(), 476, 875);
+    ctx.lineWidth = 15;
+    ctx.moveTo(160,890);
+    ctx.lineTo(800,890);
+    ctx.strokeStyle = 'red';
+    ctx.stroke();
 }
 
-function textJustification (text, x, y, maxWidth, lineSeparation) {
-    let wordsArray = text.split(' ');
+function justifyText (text, lineJumpSize, x, y) {
+    let words = text.split(' ');
+    let segmentedText = [];
     let textLine = '';
-    for(let i = 0; i < wordsArray.length; i++) {
-        let textTest = textLine + wordsArray + ' ';
-        let textWidth = ctx.measureText(textTest).width;
-        console.log(textWidth);
-        if (textWidth > maxWidth && i > 0) {
-            ctx.fillText(textLine, x, y);
-            textLine = wordsArray[i] + ' ';
-            y += lineSeparation;
+    let position = 0;
+    let actualY = y;
+
+    for (let i = 0; i < words.length; i++) {
+        textLine += words[i] + ' ';
+        let textWidth = ctx.measureText(textLine).width;
+        if (textWidth > popWidth) {
+            position ++;
+            textLine = words[i] + '';
+        }
+        if(!Array.isArray(segmentedText[position])) {
+            segmentedText[position] = new Array;
+            segmentedText[position].push(words[i]);
         } else {
-            textLine = textTest;
+            segmentedText[position].push(words[i]);
         }
     }
-    ctx.fillText(textLine, x, y);
+    let writableLine = '';
+
+    for(let row = 0; row < segmentedText.length; row++) {
+        writableLine = '';
+        for(let col = 0; col < segmentedText[row].length; col++) {
+            writableLine +=  segmentedText[row][col] + ' ';
+        }
+        ctx.fillText(writableLine, x, actualY);
+        actualY += lineJumpSize;
+    }
 }
